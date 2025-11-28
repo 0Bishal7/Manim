52c1e7911574d4017702da305cab3dcb418e615a
0Bishal7 <109542501+0Bishal7@users.noreply.github.com>
2025-11-28 22:15:23 +0530

    add new code

--- Manim_basics/basics.py
+++ Manim_basics/basics.py
@@ -1,1 +1,13 @@
 # cjcndnkcdncndcndnncdncmdmldkdkddkddksk
+from manim import *
+
+class SquareScene(Scene):
+    def construct(self):
+        # Create a square
+        square = Square()
+        
+        # Show the square on screen
+        self.play(Create(square))
+        
+        # Wait for 1 second
+        self.wait(1)
